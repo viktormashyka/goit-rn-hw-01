7ec8a09a0aeaa6d1ba799300726d6f5af56dea92
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-10-17 11:16:34 +0100

    add Roboto fonts and global colors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { color } from "./styles/global";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+  if (!fontsLoaded) {
+    <ActivityIndicator color={color.orange} />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Home screen</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ styles/global.ts
@@ -0,0 +1,8 @@
+export const color = {
+  white: "#FFFFFF",
+  black_primary: "#212121",
+  light_gray: "#F6F6F6",
+  border_gray: "#E8E8E8",
+  blue: "#1B4371",
+  orange: "#FF6C00",
+};
